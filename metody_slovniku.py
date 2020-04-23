37948fb81c0c017384154f5a7479d72bf15c34a8
Jan Meyer <j4n.meyer@gmail.com>
2020-04-23 23:33:44 +0200

    Dokončení metody slovniku - Opakovat později

--- metody_slovniku.py
+++ metody_slovniku.py
@@ -3,17 +3,22 @@ muj_slovnik = {}
 novy_slovnik = {}
 
 # vloz klice 'jméno', 'přijmení' do 'muj_slovnik' a přidej libovolne hodnoty
-muj_slovnik['jméno'] 	=    ('Karel', 'Jirka')
-muj_slovnik['přijmení'] 	=   ('Novák', 'Svoboda')
+muj_slovnik['jméno'] = 'Karel'
+muj_slovnik['přijmení']	= 'Novák'
 
 # vytvor z tuple 'muj_tuple' slovnik do slovniku 'novy_slovnik'
 muj_tuple = 'věk', 'email'
-novy_slovnik =
+novy_slovnik = novy_slovnik.fromkeys(muj_tuple)		#Metoda použitá pro vytvoření dictonary z tuple
+print(novy_slovnik)
 
 # dopln muj_slovnik o novy_slovnik
-
+muj_slovnik.update(novy_slovnik)
+print(muj_slovnik)
 
 # vypln klice 'věk' a 'mail'
+muj_slovnik['věk'] = '15'
+muj_slovnik['email']	= 'Novák@Karel.cz'
+print(muj_slovnik)
 
 
 # TADY NIC NEPIŠ!! Slouží pro kontrolu :)
